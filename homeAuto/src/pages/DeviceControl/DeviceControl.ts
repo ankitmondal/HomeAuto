fc0d9d65dac85fcfb864a519441ec46b831ec8a4
Arka Kundu <kundu.arka94@gmail.com>
2018-06-23 21:19:16 +0530

    DataBind

--- homeAuto/src/pages/DeviceControl/DeviceControl.ts
+++ homeAuto/src/pages/DeviceControl/DeviceControl.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { DeviceControlServiceProvider } from '../../providers/device-control-service/device-control-service';
+import { DeviceControlService} from '../../providers/device-control-service/device-control-service';
 @Component({
   selector: 'page-DeviceControl',
   templateUrl: 'DeviceControl.html'
 })
 export class DeviceControlPage {
-  segments = [{ "key": "bed", "value": "Bedroom" },
-  { "key": "dine", "value": "Dining" },
-  { "key": "kitchen", "value": "Kitchen" }];
-
+  // segments = [{ "key": "bed", "value": "Bedroom" },
+  // { "key": "dine", "value": "Dining" },
+  // { "key": "kitchen", "value": "Kitchen" }];
+  segments : any;
   room: string = "bed";
 
   plugs = [{ "Name": "Bed Room Plug 1", "State": "true" },
@@ -20,40 +20,64 @@ export class DeviceControlPage {
   lights = [{ "Name": "Bed Room light 1", "State": "true" },
   { "Name": "light 2", "State": "false" }];
   roomDetail = { "Plugs": this.plugs, "Fans": this.fans, "Lights": this.lights };
-  constructor(public serviceProvider: DeviceControlServiceProvider) {
+  constructor(public devserviceProvider: DeviceControlService) {
+    this.getDeviceStatus();
   }
+  deviceStatus : any;
+  getDeviceStatus() {
+    this.devserviceProvider.getDeviceStatus()
+    .subscribe(data=> this.deviceStatus=JSON.parse(data)[0]);
+      
+     console.log(this.deviceStatus);
+     this.segments = this.deviceStatus.Rooms;
+     this.room = this.deviceStatus.Rooms[0].RoomName;
+     this.roomDetail={ "Plugs": this.deviceStatus.Rooms[0].Devices[0].Plug, "Fans": this.deviceStatus.Rooms[0].Devices[0].Fan, "Lights": this.deviceStatus.Rooms[0].Devices[0].Light };
+      // this.fans = this.deviceStatus.filter((item)=> {return item.devicetype == "Fan"});
+      // this.lights = this.deviceStatus.filter((item)=> {return item.devicetype == "Light"});
+      // this.plugs = this.deviceStatus.filter((item)=> {return item.devicetype == "Plug"});
+    //console.log(this.fans);
+    
+  }
+
   onClick() {
+    this.deviceStatus.Rooms.forEach(rm => {
+      if(rm.RoomName === this.room)
+      {
+        this.roomDetail={ "Plugs": rm.Devices[0].Plug, "Fans": rm.Devices[0].Fan, "Lights": rm.Devices[0].Light };
+      }
+      
+    });
     //console.log(this.room);
-    if (this.room === "bed") {
-      this.plugs = [{ "Name": "Bed room Plug 1", "State": "true" },
-      { "Name": "Plug 2", "State": "false" }];
+    // if (this.room === "bed") {
+    //   this.plugs = [{ "Name": "Bed room Plug 1", "State": "true" },
+    //   { "Name": "Plug 2", "State": "false" }];
 
-      this.fans = [{ "Name": "Fan 1", "State": "true" },
-      { "Name": "Fan 2", "State": "false" }];
+    //   this.fans = [{ "Name": "Fan 1", "State": "true" },
+    //   { "Name": "Fan 2", "State": "false" }];
 
-      this.lights = [{ "Name": "light 1", "State": "true" },
-      { "Name": "light 2", "State": "false" }];
-    }
-    if (this.room === "dine") {
-      this.plugs = [{ "Name": "dining room Plug 1", "State": "true" },
-      { "Name": "Plug 2", "State": "false" }];
+    //   this.lights = [{ "Name": "light 1", "State": "true" },
+    //   { "Name": "light 2", "State": "false" }];
+    // }
+    // if (this.room === "dine") {
+    //   this.plugs = [{ "Name": "dining room Plug 1", "State": "true" },
+    //   { "Name": "Plug 2", "State": "false" }];
 
-      this.fans = [{ "Name": "Fan 1", "State": "true" },
-      { "Name": "Fan 2", "State": "false" }];
+    //   this.fans = [{ "Name": "Fan 1", "State": "true" },
+    //   { "Name": "Fan 2", "State": "false" }];
 
-      this.lights = [{ "Name": "light 1", "State": "true" },
-      { "Name": "light 2", "State": "false" }];
-    }
-    if (this.room === "kitchen") {
-      this.plugs = [{ "Name": "kitchen Plug 1", "State": "true" },
-      { "Name": "Plug 2", "State": "false" }];
+    //   this.lights = [{ "Name": "light 1", "State": "true" },
+    //   { "Name": "light 2", "State": "false" }];
+    // }
+    // if (this.room === "kitchen") {
+    //   this.plugs = [{ "Name": "kitchen Plug 1", "State": "true" },
+    //   { "Name": "Plug 2", "State": "false" }];
 
-      this.fans = [{ "Name": "Fan 1", "State": "true" },
-      { "Name": "Fan 2", "State": "false" }];
+    //   this.fans = [{ "Name": "Fan 1", "State": "true" },
+    //   { "Name": "Fan 2", "State": "false" }];
 
-      this.lights = [{ "Name": "light 1", "State": "true" },
-      { "Name": "light 2", "State": "false" }];
-    }
-    this.roomDetail = { "Plugs": this.plugs, "Fans": this.fans, "Lights": this.lights };
+    //   this.lights = [{ "Name": "light 1", "State": "true" },
+    //   { "Name": "light 2", "State": "false" }];
+    // }
+    // this.roomDetail = { "Plugs": this.plugs, "Fans": this.fans, "Lights": this.lights };
   }
 }
